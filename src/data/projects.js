781cd78cb2df40870c6342f5d9462bfeff8bc08e
YE LIU <tide_0106@126.com>
2021-11-03 22:12:31 -0400

    Update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,8 +8,8 @@ const data = [
     image: '/images/projects/relax-lotus.gif',
     date: '2021-03-01',
     desc:
-      'Developing a Light-Music Streaming service hoping to help the audiences relax and find inner peace in the anxious society. '
-      + 'The prototype model of the project has been rolled out, which allows the users to overlay various natrual sound tracks onto the main music track. '
+      'Created initial mock-ups and motivated developer partner to prototype web app.'
+      + ' The prototype model of the project has been rolled out, which allows the users to overlay various natrual sound tracks onto the main music track. '
       + 'More functions and music options are under development.',
   },
   {
@@ -19,11 +19,11 @@ const data = [
     date: '2021-08-30',
     desc:
       'Conducted user interviews, defined target customers and market, determined features and user flows. '
-      + '',
+      + 'Created roadmaps and mock-ups. Pitched idea to 70+ students.',
   },
   {
     title: 'HHitch',
-    subtitle: 'Ride0hailing Mobile App for students',
+    subtitle: 'Ride-hailing Mobile App for students',
     linkDemo: 'https://xd.adobe.com/view/e4496fb8-944c-422d-9123-62a2d48b4c37-b11a/',
     image: '/images/projects/HHITCH.png',
     date: '2016-12-20',
@@ -34,24 +34,20 @@ const data = [
   {
     title: 'Trip Planner',
     subtitle: 'A web app that make your vacation planning as easy as one click.',
-    linkDemo: 'https://zhukaijun0629.github.io/Small_Project-2_Speed-Typing/',
     image: '/images/projects/tripplanner.png',
     date: '2020-11-20',
     desc:
-      'Launched this addicitve mini web game "Speed-Typing" to help practice typing skills by monitoring speed and accuracy. '
-      + 'JavaScript, HTML/CSS, and Random Quotes API were used to create the game. Just try it out, you will love it!',
+      'Sometimes, planning a trip can be a headache. You need to decide where to go, plan ahead each stops and destinations, book hotels and flight tickets. Trip planning can help you ',
   },
 
   {
-    title: 'Steel Connection Design Automation',
-    subtitle: 'A comprehensive workflow to automate steel connection design',
-    linkDemo: '',
-    linkGithub: '',
-    image: '/images/projects/connection-design.png',
-    date: '2020-07-01',
+    title: 'Research Project: Two-stage Stochastic Problem (outdated)',
+    subtitle: 'A stochastic model that optimize fleet allocation and relocation',
+    linkGithub: 'https://github.com/Sofinelye/Fleet_management/blob/main/LP_CAR_ALLOCATION_with_scenarios.ipynb',
+    image: '/images/projects/fleet.png',
+    date: '2020-12-01',
     desc:
-      'Developed a comprehensive digital workflow for steel connection design, which consists of 4 phases:'
-      + 'A) Data Extraction, B) Analysis and Bucketing, C) Design Automation, and D) Result Visualization, reducing connection design project timeline by 40% ',
+      'I studied and compared two frameworks, Sample Average approximate (SAA) and Rolling Horizon (RH), to optimize a multi-stage stochastic model for the total cost, including initiation cost and operation cost after fleet size is determined. ',
   },
 ];
 
